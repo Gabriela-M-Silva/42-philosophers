ee0d490c86aaf41afe9c849a05274e3e831e3c9b
gde-mora <gde-mora@student.42sp.org.br>
2023-06-01 03:55:37 +0200

    att philo headers do c

--- philo/destroy/free_data.c
+++ philo/destroy/free_data.c
@@ -6,13 +6,13 @@
 /*   By: gde-mora <gde-mora@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 02:37:02 by gde-mora          #+#    #+#             */
-/*   Updated: 2023/05/31 02:38:53 by gde-mora         ###   ########.fr       */
+/*   Updated: 2023/06/01 03:54:55 by gde-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/philosophers.h"
 
-void	free_data(t_table *data)
+void	free_data(t_table *data) //tem comentario no header -------apagar!
 {
 	//tem q destruir os mutex
 	//e liberar as allocações
